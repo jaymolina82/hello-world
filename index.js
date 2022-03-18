9b790296359b721dbd7a21caca800bd2e869a928
C0252841 <Javier.molinamarcilla@santander.co.uk>
2022-03-18 14:11:45 +0000

    Changed message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from backend catchup!"),
+    body: JSON.stringify("Hello from Lambda and Github!"),
   }
   return response
 }
